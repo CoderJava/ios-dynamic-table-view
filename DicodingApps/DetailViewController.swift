a1098f0a57b9880defcc9b5f1ae382930f09bdd4
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-11-06 22:59:57 +0700

    Belajar membuat dynamic table view dan seque

---
+++ DicodingApps/DetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailViewController.swift
+//  DicodingApps
+//
+//  Created by Yudi Setiawan on 06/11/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var namaLabel: UILabel!
+    @IBOutlet weak var academyImage: UIImageView!
+    
+    var academy: AcademyModel? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let result = academy {
+            namaLabel.text = result.name
+            descLabel.text = result.description
+            academyImage.image = result.image
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DicodingApps/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  DicodingApps
+//
+//  Created by Yudi Setiawan on 06/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var academyTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        academyTableView.dataSource = self
+        academyTableView.delegate = self
+        
+        academyTableView.register(
+            UINib(nibName: "AcademyTableViewCell", bundle: nil), // Isi dengan nama file XIB
+            forCellReuseIdentifier: "AcademyCell" // Isi dengan Identifier Cell yang telah ditentukan
+        )
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyAcademyData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(
+            withIdentifier: "AcademyCell",
+            for: indexPath
+        ) as? AcademyTableViewCell { // Mencari UITableViewCell berdasarkan Identifier.
+            let academy = dummyAcademyData[indexPath.row]
+            cell.academyLabel.text = academy.name
+            cell.academyImageView.image = academy.image
+            return cell
+        } else {
+            return UITableViewCell() // Mengembalikan UITableViewCell jika tidak ditemukan.
+        }
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "moveToDetail", sender: dummyAcademyData[indexPath.row])
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "moveToDetail" {
+            if let detailViewController = segue.destination as? DetailViewController {
+                detailViewController.academy = sender as? AcademyModel
+            }
+        }
+    }
+}
